c6ed2c1750848ce48435316378698090e1a79bdf
kolostring <yoerodriguezjunior@gmail.com>
2026-02-15 05:47:44 -0500

    feat: rebuild migrations to avoid conflicts with supabase auth schema

--- drizzle/0001_init_tables.sql
+++ drizzle/0001_init_tables.sql
@@ -1,16 +1,12 @@
-CREATE TABLE "auth"."users" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"email" text
-);
---> statement-breakpoint
 CREATE TABLE "orders" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"client_id" uuid NOT NULL,
-	"status" text NOT NULL,
+	"status" text DEFAULT 'pending_review' NOT NULL,
 	"package_cost" integer DEFAULT 0 NOT NULL,
 	"shipping_cost" integer DEFAULT 0 NOT NULL,
 	"invested_amount" integer DEFAULT 0 NOT NULL,
 	"paid_amount" integer DEFAULT 0 NOT NULL,
+	"shop_cart_url" text NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
 	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
 );
@@ -22,13 +18,12 @@ CREATE TABLE "products" (
 	"store_order_id" text NOT NULL,
 	"url" text NOT NULL,
 	"name" text NOT NULL,
-	"tracking_number" text,
+	"tracking_number" text NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
 	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 ALTER TABLE "products" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "orders" ADD CONSTRAINT "orders_client_id_users_id_fk" FOREIGN KEY ("client_id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "products" ADD CONSTRAINT "products_order_id_orders_id_fk" FOREIGN KEY ("order_id") REFERENCES "public"."orders"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE POLICY "admin_full_access" ON "orders" AS PERMISSIVE FOR ALL TO "authenticated" USING (public.is_admin()) WITH CHECK (public.is_admin());--> statement-breakpoint
 CREATE POLICY "user_view_own_orders" ON "orders" AS PERMISSIVE FOR SELECT TO "authenticated" USING (public.get_my_id() = "orders"."client_id");--> statement-breakpoint
